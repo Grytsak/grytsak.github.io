624007e68c69621dc8d2c2db910011ebe6576c05
Grytsak <hrytsak.oleh7@gmail.com>
2023-05-27 12:28:42 +0300

    feat(portfolio): change landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './assets/scss/common.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import MainMenu from './components/MainMenu'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Technologies from './components/Technologies'
+
+library.add(fab, fas);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <MainMenu />
+        <Hero />
       </header>
+      <Technologies />
+      <Projects />
+      <Footer />
     </div>
   );
 }
